56a396a13aaa27d9426020de7ddc098005d66dcf
Casper <crask21@student.sdu.dk>
2022-10-14 10:37:44 +0200

    Windows sound test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+
+#include "stdafx.h"
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "\a\a\a\a\a\a\a\a";
+	Beep (500,50);
+	return 0;
+}
+
+/*
 #include <iostream>
 int main(int argc, char const *argv[])
 {
     std::cout<< "Hello Deez nuts" << std::endl;
     return 0;
 }
+*/
