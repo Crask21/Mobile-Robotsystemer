1ad6f5082f5a82ab5db4bb774d1f4462d52787d1
markus simonsen <markus.k.simonsen@gmail.com>
2022-11-08 14:57:41 +0100

    updates

--- signalProcessing/withoutPlot.py
+++ signalProcessing/withoutPlot.py
@@ -4,56 +4,33 @@ import numpy as np
 import time
 from scipy.fftpack import fft
 
-def dtmf_to_hexa(inputList):
+dtmf_freq = [[1209,697], # 0
+                    [1336,697],  # 1
+                    [1477,697],  # 2
+                    [1633,697],  # 3
+                    [1209,770],  # 4
+                    [1336,770],  # 5
+                    [1477,770],  # 6
+                    [1633,770],  # 7
+                    [1209,852],  # 8
+                    [1336,852],  # 9
+                    [1477,852],  # A
+                    [1633,852],  # B
+                    [1209,941],  # C
+                    [1336,941],  # D
+                    [1477,941],  # E
+                    [1633,941]]  # F
 
-    output = []
+upperRange= 20
+lowerRange=20
 
-    for i in range(len(inputList)):
-        if(inputList[0]>inputList[1]):
-            temp = inputList[0]
-            inputList[0] = inputList[1]
-            inputList[1] = temp
+def dtmf_to_hexa(inputFreqs):
 
-    input1 = inputList[0]
-    input2 = inputList[1]
-
-    if((input1>=650) and (input1<=730)):
-        if((input2>=1150) and (input2<=1250)):
-            output.append(0)
-        elif((input2>=1280) and (input2<=1380)):
-            output.append(1)
-        elif((input2>=1420) and (input2<=1520)):
-            output.append(2)
-        elif((input2>=1580) and (input2<=1680)):
-            output.append(3)
-    elif((input1>=745) and (input1<=810)):
-        if((input2>=1150) and (input2<=1250)):
-            output.append(4)
-        elif((input2>=1280) and (input2<=1380)):
-            output.append(5)
-        elif((input2>=1420) and (input2<=1520)):
-            output.append(6)
-        elif((input2>=1580) and (input2<=1680)):
-            output.append(7)
-    elif((input1>=820) and (input1<=890)):
-        if((input2>=1150) and (input2<=1250)):
-            output.append(8)
-        elif((input2>=1280) and (input2<=1380)):
-            output.append(9)
-        elif((input2>=1420) and (input2<=1520)):
-            output.append('A')
-        elif((input2>=1580) and (input2<=1680)):
-            output.append('B')
-    elif((input1>=900) and (input1<=980)):
-        if((input2>=1150) and (input2<=1250)):
-            output.append('C')
-        elif((input2>=1280) and (input2<=1380)):
-            output.append('D')
-        elif((input2>=1420) and (input2<=1520)):
-            output.append('E')
-        elif((input2>=1580) and (input2<=1680)):
-            output.append('F')
-
+    output =[]
+    inputFreqs.sort()
+    for i in range(16):
+        if (inputFreqs[0]<dtmf_freq[i][1]+upperRange and inputFreqs[0]>dtmf_freq[i][1]-lowerRange)and(inputFreqs[1]<dtmf_freq[i][0]+upperRange and inputFreqs[1]>dtmf_freq[i][0]-lowerRange):
+            output= [i]
     return output
 
 #frequencies to delete
@@ -125,7 +102,7 @@ while True:
     
     if freqMagn[highestFreqs[1]]>1:
         outputList=outputList+dtmf_to_hexa(xf[highestFreqs])
-    print(outputList)
+    print(hex(outputList))
     print("------------------------------------")
     loopEnd=time.time()
     if loopEnd-loopStart>baudTime:
@@ -134,12 +111,12 @@ while True:
         loopEnd=time.time()
     print(loopEnd-loopStart)
     if len(outputList)>0:
-        if outputList[0]!="A":
+        if outputList[0]!=0xa:
             outputList=[]
-    if outputList==["A","B","C"]:
+    if outputList==[0xa,0xb,0xc]:
         print("Synchronization is succesfull")
         outputList=[]
-    if outputList!=["A","B","C"] and len(outputList)==3:
+    if outputList!=[0xa,0xb,0xc] and len(outputList)==3:
         print("Synchronization commensing, delaying with 10 percent sec")
         outputList=[]
         while loopEnd-loopStart<baudTime+baudTime*0.1:
