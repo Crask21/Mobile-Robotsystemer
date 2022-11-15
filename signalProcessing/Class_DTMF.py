8013908a622816219d75d5a10070e72cfe1ebcc4
anpom21 <anpom21@student.sdu.dk>
2022-11-15 15:39:04 +0100

    ye

--- signalProcessing/Class_DTMF.py
+++ signalProcessing/Class_DTMF.py
@@ -182,18 +182,33 @@ class DTMF:
 fs = 44100
 amplitude = 5000
 media = 'PyGame' # 'SD'
-fade_P = 0.02
-baud_rate = 15
+fade_P = 0.25
+baud_rate = 10
+sync_num = 10
 
 # Initialization
 data_P = DTMF(fs, amplitude, fade_P, baud_rate, media)
 
-r_seq = []
+# Synchroniazation
+sync = []
+for i in np.arange(sync_num):
+    sync.append(0xA)
+    sync.append(0xB)
+sync.append(0xC)
+sync.append(0xC)
+
+print(sync)
+
+# Random package
+size = 16
+random_data = []
 for i in np.arange(16):
-    r_seq.append(randrange(16))
-print(r_seq)
+    random_data.append(randrange(size))
+print(random_data)
 
 
+
+print(hex(128))
 
 # Plot FFT
     #data_P.send_package([0xC])
@@ -208,8 +223,8 @@ print(r_seq)
 
 
 def thread_f():
-    data_P.send_package([0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xC,0xC])
-    data_P.send_package(r_seq)
+    data_P.send_package(sync)
+    data_P.send_package(random_data)
 
 
 
