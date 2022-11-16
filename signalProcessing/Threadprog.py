290a65c8da1eb5b95817e5a3df9f2b0b7bb68e08
markus simonsen <markus.k.simonsen@gmail.com>
2022-11-16 12:49:42 +0100

    Structured Threadprog.py better

--- signalProcessing/Threadprog.py
+++ signalProcessing/Threadprog.py
@@ -6,38 +6,37 @@ from scipy.fftpack import fft
 import matplotlib.pyplot as plt
 
 from scipy.signal import butter, lfilter
+from scipy.signal import freqz
 
-#------------------------FUNCTIONS--------------------------------
+#--------------------------------VARIABLES--------------------------------
 
 
+LOWCUT = 200
+HIGHCUT = 3000
 
-def butter_bandpass(lowcut, highcut, fs, order=5):
+resolution=int(10)
+baudRate=500
+
+FORMAT = pyaudio.paInt16 
+CHANNELS = 1
+RATE = 15000
+INPUT_BLOCK_TIME = 1
+INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
+
+#--------------------------------FUNCTIONS--------------------------------
+def butter_bandpass(LOWCUT, HIGHCUT, fs, order=5):
     nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
+    low = LOWCUT / nyq
+    high = HIGHCUT / nyq
     b, a = butter(order, [low, high], btype='band')
     return b, a
 
-fs = 10000
-lowcut = 500
-highcut = 1700
-
-def butter_bandpass_filter(input, lowcut=lowcut, highcut=highcut, fs=fs, order=3):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+def butter_bandpass_filter(input, LOWCUT=LOWCUT, HIGHCUT=HIGHCUT, fs=RATE, order=5):
+    b, a = butter_bandpass(LOWCUT, HIGHCUT, fs, order=order)
     y = lfilter(b, a, input)
     return y
 
-baudRate=10
-    
-resolution=int(10)
-
-FORMAT = pyaudio.paInt16 
-#SHORT_NORMALIZE = (1.0/32768.0)
-CHANNELS = 1
-#RATE = 44100
-RATE = fs
-INPUT_BLOCK_TIME = 1
-INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
+#------------------------------------PYAUDIO-----------------------------------
 # pyaudio class instance
 p = pyaudio.PyAudio()
 
@@ -61,8 +60,6 @@ format = "%dh"%(count)
 
 data_int = np.array(struct.unpack(format, data))
 
-#data_int=butter_bandpass_filter(data_int)
-
 yf=fft(data_int)
 yf=abs(yf)
 yf=np.delete(yf,delList)
@@ -73,32 +70,16 @@ data = stream.read(int(RATE/(resolution*baudRate)))
 count = len(data)/2
 format = "%dh"%(count)
 
-count=0
 while True:
     #divided by baudRate too to get the movement of the window
-    #start=time.time()
     data = stream.read(int(RATE/(resolution*baudRate)))
-    #end=time.time()
-    #print(end-start)
     data_nex = np.array(struct.unpack(format, data))
     #remove the first points in the array
     data_int=np.delete(data_int,np.arange(int(RATE/(resolution*baudRate))))
     data_int=np.append(data_int,data_nex)
     
-    data_int=butter_bandpass_filter(data_int)
+    #data_int=butter_bandpass_filter(data_int)
 
     yf=fft(data_int)
     yf=np.delete(yf,delList)
     
-    print(xf[np.argmax(yf)])
-    #end=time.time()
-    #print("a:"+str(end-start))
-    #count+=1
-    #if count%10==0:
-    #    print(xf[np.argmax(yf)])
-    
-
-
-
-
-#threading.Thread(target=listen)
