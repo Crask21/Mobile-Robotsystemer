02cb6e9314ec702934c9d80ea467447f824b225e
Andreas <anpom21@student.sdu.dk>
2022-11-16 18:16:44 +0100

    optimization

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from Send_DTMF.Class_DTMF import DTMF
 from Send_DTMF.Class_DTMF import synchroniazation
 from Send_DTMF.Class_DTMF import rand_pack
-#import protocol
+from protocol import protocol_In
+from protocol import protocol_Out
+
+
+
 
 
 # DTMF Settings
 fs = 44100
 amplitude = 5000
-media = 'PyGame' # 'SD'
-fade_P = 0.02
-baud_rate = 2
+fade_P = 0.01
+baud_rate = 30
+
+
 
 # Initialization
-dtmf = DTMF(fs, amplitude, fade_P, baud_rate, media)
+dtmf = DTMF(fs, amplitude, fade_P, baud_rate, 'PyGame')
 
 
 
-dtmf.send_package(synchroniazation(10))
 
 
 
