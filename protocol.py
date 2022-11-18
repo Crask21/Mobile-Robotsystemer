ffbca02da356dbf72eb0f9c7d9a8c870d1344f50
Nikolai Prip <nikoprip@gmail.com>
2022-11-18 10:36:57 +0100

    Changed to functions

--- protocol.py
+++ protocol.py
@@ -1,16 +1,12 @@
 import numpy as np
 
-print("Hvilken besked vil du sende?")
-contents = input()
+with open('test.txt') as f:
+    contents = f.read()
 
-input1 = [[20,10],[-10,30],[contents]]
+input_list = [[20,10],[-10,30],[contents]]
 
-def protocol_In(input_List):
+def convert_to_hexa(input_List):
     temp_main = []
-    temp_main2 = []
-    count = 0
-    output = []
-
     for i in range(len(input_List)):
         temp = []
         for j in range(len(input_List[i])):
@@ -20,47 +16,162 @@ def protocol_In(input_List):
                 for k in range(len(input_List[i][j])):
                     temp.append(hex(ord(input_List[i][j][k])))
         temp_main.append(temp)
+    return temp_main
 
-    for i in range(len(temp_main)):
-        if len(temp_main[i])>2:
+def hexa_devide(input_List):
+    temp_main = []
+    for i in range(len(input_List)):
+        if len(input_List[i])>2:
+            temp = []
+            for j in range(len(input_List[i])):
+                temp.append(hex(int(input_List[i][j][2],16)))
+                temp.append(hex(int(input_List[i][j][3],16)))
+            temp_main.append(temp)
+        elif len(input_List[i])==2:
             temp1 = []
-            for j in range(len(temp_main[i])):
-                temp1.append(hex(int(temp_main[i][j][2],16)))
-                temp1.append(hex(int(temp_main[i][j][3],16)))
-            temp_main2.append(temp1)
-        elif len(temp_main[i])==2:
-            temp2 = []
-            for j in range(len(temp_main[i])):
-                if len(temp_main[i][j])==3:
-                    temp2.append(hex(0))
-                    temp2.append(hex(int(temp_main[i][j][2],16)))
-                elif len(temp_main[i][j])==4:
-                    temp2.append(hex(int(temp_main[i][j][2],16)))
-                    temp2.append(hex(int(temp_main[i][j][3],16)))
-            temp_main2.append(temp2)
+            for j in range(len(input_List[i])):
+                if len(input_List[i][j])==3:
+                    temp1.append(hex(0))
+                    temp1.append(hex(int(input_List[i][j][2],16)))
+                elif len(input_List[i][j])==4:
+                    temp1.append(hex(int(input_List[i][j][2],16)))
+                    temp1.append(hex(int(input_List[i][j][3],16)))
+            temp_main.append(temp1)
+    return temp_main
 
-    for i in range(len(temp_main2)):
-        for j in range(len(temp_main2[i])-1):
-            if temp_main2[i][j]=='0x0' and temp_main2[i][j+1]=='0x1':
-                temp_main2[i].insert(j,'0xf')
-                temp_main2[i].insert(j,'0xf')
+def add_esc(input_List):
+    for i in range(len(input_List)):
+        for j in range(len(input_List[i])-1):
+            if input_List[i][j]=='0x0' and input_List[i][j+1]=='0x1':
+                input_List[i].insert(j,'0xf')
+                input_List[i].insert(j,'0xf')
+    return input_List
 
-    for i in range(len(temp_main2)):
+def add_seq(input_List):
+    count = 0
+    for i in range(len(input_List)):
         count=count+1
-        temp_main2[i].insert(0,hex(count))
-    
-    for i in range(len(temp_main2)):
-        temp_main2[i].insert(0,'0x1')
-        temp_main2[i].insert(0,'0x0')
-        temp_main2[i].insert(len(temp_main2[i]),'0x0')
-        temp_main2[i].insert(len(temp_main2[i]),'0x1')
-    
-    for i in range(len(temp_main2)):
-        for j in range(len(temp_main2[i])):
-            output.append(temp_main2[i][j])
+        input_List[i].insert(0,hex(count))
+    return input_List
 
+def add_StartStop(Input_List):
+    for i in range(len(Input_List)):
+        Input_List[i].insert(0,'0x1')
+        Input_List[i].insert(0,'0x0')
+        Input_List[i].insert(len(Input_List[i]),'0x0')
+        Input_List[i].insert(len(Input_List[i]),'0x1')
+    return Input_List
+
+def one_list(input_List):
+    output = []
+    for i in range(len(input_List)):
+        for j in range(len(input_List[i])):
+            output.append(input_List[i][j])
     return output
 
+def organize(input_List):
+    check = 0
+    output = []
+    for i in range(len(input_List)-1):
+        if input_List[i]=='0x0' and input_List[i+1]=='0x1':
+            if input_List[i-1]!='0xF' and input_List[i-2]!='0xf':
+                check = check+1
+                if check % 2 == 1:
+                    temp = []
+                    for j in np.arange(i+2,len(input_List)):
+                        if input_List[j]=='0x0' and input_List[j+1]=='0x1':
+                            if input_List[j-1]=='0xf' and input_List[j-2]=='0xf':
+                                temp.append(input_List[j])
+                            else:
+                                output.append(temp)
+                                break
+                        else:
+                            temp.append(input_List[j])
+    return output
+
+def esc_check(inpt_List):
+    output = []
+    for k in range(len(inpt_List)):
+        temp = []
+        for l in range(len(inpt_List[k])-1):
+            if inpt_List[k][l]=='0xf':
+                if inpt_List[k][l+1]=='0xf':
+                    continue
+                elif inpt_List[k][l-1]=='0xf':
+                    continue
+                else:
+                    temp.append(inpt_List[k][l])
+            else:
+                temp.append(inpt_List[k][l])
+        temp.append(inpt_List[k][len(inpt_List[k])-1])
+        output.append(temp)
+    return output
+
+def remove_seq(input_List):
+    for i in range(len(input_List)):
+        input_List[i].pop(0)
+    return input_List
+
+def convert_to_decimal(input_List):
+    output = []
+    for i in range(len(input_List)):
+        count = 0
+        temp = []
+        if len(input_List[i])<=4:
+            for j in range(len(input_List[i])-1):
+                if count % 2 == 0:
+                    temp.append((16*int(input_List[i][j],16)+int(input_List[i][j+1],16))-128)
+                    count=count+1
+                else:
+                    count=count+1
+            output.append(temp)
+        else:
+            output.append(input_List[i])
+    return output
+
+def hexa_to_msg(dtmf_signal):
+    temp_list = []
+    message = ""
+    for i in range(len(dtmf_signal)):
+        count = 0
+        if len(dtmf_signal[i])>2:
+            for j in range(len(dtmf_signal[i])-1):
+                if count % 2 == 0:
+                    temp_list.append(hex(16*int(dtmf_signal[i][j],16)+int(dtmf_signal[i][j+1],16)))
+                    count = count+1
+                else:
+                    count = count+1
+    for i in range(len(temp_list)):
+        message = message+chr(int(temp_list[i],16))
+
+    return message
+
+def movement(input_List):
+    move = []
+    for i in range(len(input_List)):
+        if len(input_List[i])==2:
+            move.append(input_List[i])
+    return move
+
+#Add protocol
+input_list=convert_to_hexa(input_list)
+input_list=hexa_devide(input_list)
+input_list=add_esc(input_list)
+input_list=add_seq(input_list)
+input_list=add_StartStop(input_list)
+input_list=one_list(input_list)
+input_list=organize(input_list)
+input_list=esc_check(input_list)
+input_list=remove_seq(input_list)
+input_list=convert_to_decimal(input_list)
+print(input_list)
+print(hexa_to_msg(input_list))
+print(movement(input_list))
+
+#Decode protocol
+
+
+''' 
 def protocol_Out(hexaList):
     output = []
     move = []
@@ -68,60 +179,42 @@ def protocol_Out(hexaList):
     message = []
     tempOut=[]
     check = 0
-    for i in range(len(hexaList)-1):
-        if hexaList[i]=='0x0' and hexaList[i+1]=='0x1':
-            if hexaList[i-1]!='0xF' and hexaList[i-2]!='0xf':
-                check = check+1
-                if check % 2 == 1:
-                    temp = []
-                    for j in np.arange(i+2,len(hexaList)):
-                        if hexaList[j]=='0x0' and hexaList[j+1]=='0x1':
-                            if hexaList[j-1]=='0xf' and hexaList[j-2]=='0xf':
-                                temp.append(hexaList[j])
-                            else:
-                                tempOut.append(temp)
-                                break
-                        else:
-                            temp.append(hexaList[j])
-    
-    for k in range(len(tempOut)):
-        temp1 = []
-        for l in range(len(tempOut[k])-1):
-            if tempOut[k][l]=='0xf':
-                if tempOut[k][l+1]=='0xf':
-                    continue
-                elif tempOut[k][l-1]=='0xf':
-                    continue
-                else:
-                    temp1.append(tempOut[k][l])
-            else:
-                temp1.append(tempOut[k][l])
-        temp1.append(tempOut[k][4])
-        output.append(temp1)
-    print(output)
-    for i in range(len(output)):
-        output[i].pop(0)
-    
-    for i in range(len(output)):
-        count = 0
-        new_list = []
-        if len(output[i])<=4:
-            for j in range(len(output[i])-1):
-                if count % 2 == 0:
-                    new_list.append((16*int(output[i][j],16)+int(output[i][j+1],16))-128)
-                    count=count+1
-                else:
-                    count=count+1
-            output2.append(new_list)
-        else:
-            output2.append(output[i])
 
     for i in range(len(output2)):
         if len(output2[i])==2:
             move.append(output2[i])
         else:
             message=output2[i]
+    
+    return output2
 
-    return move
 #print(protocol_In(input1))
-print(protocol_Out(protocol_In(input1)))
+#print(protocol_Out(protocol_In(input1)))
+
+def translate(dtmf_signal):
+    temp_list = []
+    message = ""
+    for i in range(len(dtmf_signal)):
+        count = 0
+        if len(dtmf_signal[i])>2:
+            for j in range(len(dtmf_signal[i])-1):
+                if count % 2 == 0:
+                    temp_list.append(hex(16*int(dtmf_signal[i][j],16)+int(dtmf_signal[i][j+1],16)))
+                    count = count+1
+                else:
+                    count = count+1
+    for i in range(len(temp_list)):
+        message = message+chr(int(temp_list[i],16))
+
+    return message
+
+def movement(dtmf_signal):
+    move = []
+    for i in range(len(dtmf_signal)):
+        if len(dtmf_signal[i])==2:
+            move.append(dtmf_signal[i])
+    return move
+
+print(translate(protocol_Out(protocol_In(input1))))
+print(movement((protocol_Out(protocol_In(input1)))))
+'''
