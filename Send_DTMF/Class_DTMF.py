46fafdc759f8d2ae511a04ce1e4eb64ba11e7af2
anpom21 <anpom21@student.sdu.dk>
2022-11-18 12:18:05 +0100

    compare method

--- Send_DTMF/Class_DTMF.py
+++ Send_DTMF/Class_DTMF.py
@@ -227,6 +227,33 @@ class SEND:
         print(random_data)
         return random_data
 
+    def compare(data,original, recieved):
+        if original == recieved:
+            print('100% match')
+        else:
+            send_count =[]
+            for i in range(16):
+                send_count.append(original.count(i))
+
+
+
+
+            recieved_count = []
+            for i in range(16):
+                recieved_count.append(recieved.count(i))
+
+            count = 0
+            for i in range(16):
+                
+                if recieved_count[i] == send_count[i]:
+                    count += 1
+
+
+
+            print(count/16*100,'% count match')
+
+
+
 
 
 
