875aad9e0c9b63ca8b869d5cc4597fee244f4f46
markus simonsen <markus.k.simonsen@gmail.com>
2022-11-18 10:05:07 +0100

    overclass

--- DTMF/DTMF.py
+++ DTMF/DTMF.py
@@ -1,3 +1,12 @@
 from signalProcessing.Threadprog import LISTEN
-from Send_DTMF.Class_DTMF import DTMF
+from Send_DTMF.Class_DTMF import SEND
+
+class DTMF:
+
+    # DTMF Settings
+    fs = 44100
+    amplitude = 5000
+    media = 'PyGame' # 'SD'
+    fade_P = 0.02
+    baud_rate = 10
 
