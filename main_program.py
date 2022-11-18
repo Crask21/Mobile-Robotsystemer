6855fac262f298b122cb2b34a224a275e3510ad8
markus simonsen <markus.k.simonsen@gmail.com>
2022-11-18 13:59:14 +0100

    stuff

--- main_program.py
+++ main_program.py
@@ -1,7 +1,14 @@
 from DTMF.DTMF_overclass import DTMF
+import time
+
 
 robot=DTMF(10)
-robot.listen.startListen()
-pack=robot.send.rand_pack(10)
+#robot.listen.startListen()
+pack=[0, 1, 1, 10, 2, 0, 3, 0, 1, 0, 1, 2, 13, 4, 7, 0, 0, 1, 0, 1, 3, 9, 13, 10, 4, 0, 1, 0, 1, 4, 7, 0, 6, 9, 2, 0, 3, 13, 2, 0, 3, 3, 0, 1]
 
-robot.send.send_package([*robot.send.synchroniazation(10),*pack])
+robot.send.send_package([*robot.send.synchroniazation(14,'mute'),*pack])
+robot.listen.startListen()
+print(pack)
+
+time.sleep(10)
+print(robot.listen.outputList==pack)
