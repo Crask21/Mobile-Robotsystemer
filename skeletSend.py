e4fcb0a43fd688538677a2a1f8e39075012fecff
Casper <crask21@student.sdu.dk>
2022-11-22 17:44:38 +0100

    Skelet first go

---
+++ skeletSend.py
@@ -0,0 +1,31 @@
+import sys
+import Turtlebot.Turtlebot_Python.moveClass as moveClass
+import signalProcessing.protocol as protocol
+from DTMF.DTMF_overclass import DTMF
+import time
+
+def main():
+    robot=DTMF(20)
+    #moveObj = moveClass.bot()
+    
+    with open('file.txt') as f:
+        contents = f.read()
+    move = [[20,10],[-10,30]]
+    list = move + [[contents]]
+    
+    list = protocol.convert_to_hexa(list)
+    list = protocol.hexa_devide(list)
+    list = protocol.add_seq(list)
+    list = protocol.add_address(list)
+    list = protocol.add_CRC(list)
+    list = protocol.add_esc(list)
+    list = protocol.add_StartStop(list)
+    list = protocol.one_list(list)
+    
+    robot.send.send_package([*robot.send.synchroniazation(150,'mute'),*pack])
+    
+    #moveObj.move(ang, dist)
+    #moveObj.stop()
+
+if __name__ == "__main__":
+    main()
