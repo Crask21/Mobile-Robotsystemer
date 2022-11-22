34ece2b5acd2d429d4b85f72e7271dea5a58d871
anpom21 <anpom21@student.sdu.dk>
2022-11-22 13:54:11 +0100

    Compare

--- signalProcessing/Class_DTMF.py
+++ signalProcessing/Class_DTMF.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 #import sounddevice as sd
@@ -8,51 +9,26 @@ import threading
 
 
 
-#dtmf_freq = [[1209,697], # 0
-#                    [1336,697],  # 1
-#                    [1477,697],  # 2
-#                    [1633,697],  # 3
-#                    [1209,770],  # 4
-#                    [1336,770],  # 5
-#                    [1477,770],  # 6
-#                    [1633,770],  # 7
-#                    [1209,852],  # 8
-#                    [1336,852],  # 9
-#                    [1477,852],  # A
-#                    [1633,852],  # B
-#                    [1209,941],  # C
-#                    [1336,941],  # D
-#                    [1477,941],  # E
-#                    [1633,941]]  # F
+dtmf_freq = [[1209,697], # 0
+                    [1336,697],  # 1
+                    [1477,697],  # 2
+                    [1633,697],  # 3
+                    [1209,770],  # 4
+                    [1336,770],  # 5
+                    [1477,770],  # 6
+                    [1633,770],  # 7
+                    [1209,852],  # 8
+                    [1336,852],  # 9
+                    [1477,852],  # A
+                    [1633,852],  # B
+                    [1209,941],  # C
+                    [1336,941],  # D
+                    [1477,941],  # E
+                    [1633,941]]  # F
 
-def CharListToInt(list):
-    hex_dict = {
-            '0' : 0x0,
-            '1' : 0x1,
-            '2' : 0x2,
-            '3' : 0x3,
-            '4' : 0x4,
-            '5' : 0x5,
-            '6' : 0x6,
-            '7' : 0x7,
-            '8' : 0x8,
-            '9' : 0x9,
-            'a' : 0xA,
-            'b' : 0xB,
-            'c' : 0xC,
-            'd' : 0xD,
-            'e' : 0xE,
-            'f' : 0xF
-            }
-        
-    res =  []
-    for i in range(len(list)):
-        res.append(hex_dict[list[i][-1]])
-    
-    return res
 
-###gg
-class SEND:
+
+class DTMF:
 
     def __init__(data, fs, amplitude, p_fade, baud, sound_media = 'PyGame'):
 
@@ -66,10 +42,9 @@ class SEND:
         data.duration = 1/baud
         data.sound_media = sound_media
 
-        
-        
-       
-        data.FFT = []
+        # Initialize sound array
+        data.soundwave = np.array([])
+        data.FFT = 0
 
         # Initialize DMTF tone list
         data.dtmf = []
@@ -78,9 +53,9 @@ class SEND:
 
 
 # Send package of hexi decimals
-    def package(data, package, mute = False):
+    def send_package(data, package):
 
-
+        
         data.soundwave = np.arange(0,1)
 
         # Convert package into sound array
@@ -90,19 +65,16 @@ class SEND:
             # Delete end spike
             data.soundwave[-1] = 0
 
+        
 
-        if not mute and data.sound_media == 'PyGame':
-            # Play through PyGame
+        # Play through PyGame
+        if data.sound_media == 'PyGame':
             data.play_PyGame(data.soundwave)
-
-            # Play through Sounddevice
-        elif not mute and data.sound_media == 'SD':
+        
+        # Play through Sounddevice
+        elif data.sound_media == 'SD':
             data.play_SD(data.soundwave)
-
-
-    def send_package(data, package, mute = False):
-        play_package = threading.Thread(target=data.package, args=(package, mute))
-        play_package.start()
+           
 
 # Plot the package as DTMF tones
     def plot_last_package(data):
@@ -110,10 +82,7 @@ class SEND:
         package_size = round(len(data.soundwave)/data.fs/data.duration)
 
         time = np.arange(0, data.duration * package_size, 1/data.fs)
-        for i in range(34):
-            data.soundwave = np.delete(data.soundwave,-1)
-
-        
+        data.soundwave = np.delete(data.soundwave,-1)
 
         plt.plot(time,data.soundwave,'r--')
         plt.ylabel('some numbers')
@@ -146,7 +115,7 @@ class SEND:
             fade = np.linspace(0,1,num=number_of_faded_points)
             fade_end = np.linspace(1,0,num=number_of_faded_points)
 
-            data.FFT.append(np.fft.fft(xi))
+            data.FFT = np.fft.fft(xi)
 
             for j in np.arange(number_of_faded_points):
                 xi[j] = xi[j] * fade[j]
@@ -208,62 +177,116 @@ class SEND:
     #    wav_wave = np.array(soundwave, dtype=np.int16)
     #    sd.play(wav_wave, blocking=True)
 
-    # Synchroniazation
-    def synchroniazation(data,num, mute = False):
-        sync = []
-        for i in range(num):
-            sync.append(0xA)
-            sync.append(0xB)
-        sync.append(0xC)
-        sync.append(0xC)
-        #data.send_package(sync,mute)
-        return sync
 
-        # Random package
-    def rand_pack(data,num):
-        size = 16
-        random_data = []
-        for i in range(num):
-            random_data.append(randrange(size))
-        print(random_data)
-        return random_data
+# DTMF Settings
+fs = 44100
+amplitude = 5000
+media = 'PyGame' # 'SD'
+fade_P = 0.02
+baud_rate = 0.1
+sync_num = 
 
+# Initialization
+data_P = DTMF(fs, amplitude, fade_P, baud_rate, media)
 
+# Synchroniazation
+sync = []
+for i in range(sync_num):
+    sync.append(0xA)
+    sync.append(0xB)
+sync.append(0xC)
+sync.append(0xC)
 
-#ff
+print(sync)
 
-    def compare(data,original, recieved):
-        if len(recieved) > len(original):
-            dif = len(recieved) - len(original)
-            recieved = recieved[:len(recieved) - dif]
-
-        if original == recieved:
-            print('100% match')
-        else:
-            send_count =[]
-            for i in range(16):
-                send_count.append(original.count(i))
-
-
-
-
-            recieved_count = []
-            for i in range(16):
-                recieved_count.append(recieved.count(i))
-
-            count = 0
-            for i in range(16):
-                
-                if recieved_count[i] == send_count[i]:
-                    count += 1
-
-
-
-            print(count/16*100,'% count match')
-            print(original)
-            print(recieved)
+# Random package
+size = 16
+random_data = []
+for i in range(size):
+    random_data.append(randrange(size))
+print(random_data)
 
 
 
 
 
+# Plot FFT
+    #data_P.send_package([0xC])
+    #data_P.plot_fft()
+
+# Send custom freq
+    #sound = data_P.makeDTMF(amplitude,1/baud_rate,dtmf_freq[0xC][1],dtmf_freq[0xC][0],fs,fade_P)
+    #data_P.play_PyGame(sound)
+
+data_P.send_package([0x6,0x6,0x6,0x6])
+
+
+#def thread_f():
+#    data_P.send_package(sync)
+#    data_P.send_package(random_data)
+
+
+
+
+
+#play_package = threading.Thread(target=thread_f, args=())
+
+#play_package.start()
+
+while True:
+    True
+
+while True:
+    loopstart = time.time()
+    data_P.send_package([0xA,0xB])
+    loopend = time.time()
+    #print(loopend-loopstart)
+
+    
+     #dtmf_freq = [[1209,697], # 0
+  #                  [1336,697],  # 1
+  #                  [1477,697],  # 2
+  #                  [1633,697],  # 3
+  #                  [1209,770],  # 4
+  #                  [1336,770],  # 5
+  #                  [1477,770],  # 6
+  #                  [1633,770],  # 7
+  #                  [1209,852],  # 8
+  #                  [1336,852],  # 9
+  #                  [1477,852],  # A
+  #                  [1633,852],  # B
+  #                  [1209,941],  # C
+  #                  [1336,941],  # D
+  #                  [1477,941],  # E
+  #                  [1633,941]]  # F
+
+#       SEND DTMF THREAD
+#       SEND DTMF THREAD
+#       SEND DTMF THREAD
+#       SEND DTMF THREAD
+#       SEND DTMF THREAD
+#       SEND DTMF THREAD
+#       SEND DTMF THREAD
+
+# DTMF Settings
+fs = 44100
+amplitude = 5000
+media = 'PyGame' # 'SD'
+fade_P = 0.02
+baud_rate = 15
+
+# Initialization
+data_P = DTMF(fs, amplitude, fade_P, baud_rate, media)
+
+r_seq = []
+for i in np.arange(16):
+    r_seq.append(randrange(16))
+print(r_seq)
+
+def thread_f():
+    data_P.send_package([0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xC,0xC])
+    data_P.send_package(r_seq)
+
+play_package = threading.Thread(target=thread_f, args=())
+
+play_package.start()
