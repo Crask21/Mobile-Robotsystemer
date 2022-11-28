1b78a2ca43f9e38a03f7b3366fece202de7a59f9
markus simonsen <markus.k.simonsen@gmail.com>
2022-11-28 11:18:58 +0100

    change parameter

--- skeletReceive.py
+++ skeletReceive.py
@@ -7,7 +7,7 @@ import time
 def main():
     pack=[0, 1, 10, 11, 12, 1, 9, 4, 8, 10, 4, 0, 1, 0, 1, 10, 11, 12, 2, 7, 6, 9, 14, 5, 0, 1, 0, 1, 10, 11, 12, 3, 4, 4, 4, 5, 4, 5, 5, 10, 2, 0, 4, 14, 5, 5, 5, 4, 5, 3, 11, 0, 1]
     #print(len(pack))
-    robot=DTMF(20,10)
+    robot=DTMF(50,10)
     moveObj = bot()
     list = robot.listen.startListen()
     list = protocol.organize(list)
