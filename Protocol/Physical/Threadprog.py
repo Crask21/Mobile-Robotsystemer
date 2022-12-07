0090d67caed6f41916e1c27ab33bd86cdf9e4033
markus simonsen <markus.k.simonsen@gmail.com>
2022-12-07 15:49:13 +0100

    aswq

--- Protocol/Physical/Threadprog.py
+++ Protocol/Physical/Threadprog.py
@@ -54,7 +54,7 @@ class LISTEN():
         rec.upperRange=20
         rec.lowerRange=20
         rec.outputList=np.array([])
-        rec.noise_level=2000
+        rec.noise_level=1500
 
         #-----------------------------------FREQUENCIES------------------------------------------
         #resolution is defined as fs/(points worked on)
