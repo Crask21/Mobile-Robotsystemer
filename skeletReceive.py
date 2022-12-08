9eb597461746945d9eaa50b36fb83b302250d7e8
anpom21 <anpom21@student.sdu.dk>
2022-12-08 13:47:41 +0100

    aps

--- skeletReceive.py
+++ skeletReceive.py
@@ -8,7 +8,7 @@ def main():
     pack= [0, 1, 10, 11, 12, 1, 8, 0, 14, 4, 6, 0, 13, 0, 1, 0, 1, 10, 11, 12, 2, 13, 10, 14, 4, 6, 15, 15, 0, 1, 0, 1, 0, 1, 10, 11, 12, 3, 4, 4, 6, 5, 6, 5, 7, 10, 2, 0, 6, 14, 2, 8, 9, 0, 1, 0, 1, 10, 11, 12, 4, 7, 5, 7, 4, 7, 3, 12, 10, 10, 0, 1]
     #print(len(pack))
     global robot
-    robot=DTMF(50,10, mono_robot = True)
+    robot=DTMF(50,30, mono_robot = True)
     # for testing
     #data_prot = protocolClass(moves=pack,robot=robot)
     #data_prot.DataLinkUp()  
