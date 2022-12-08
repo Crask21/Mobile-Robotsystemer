ecd093c5d9b9803222ddba55d6b0bfee76936753
anpom21 <anpom21@student.sdu.dk>
2022-12-08 14:05:20 +0100

    aps

--- skeletReceive.py
+++ skeletReceive.py
@@ -1,5 +1,5 @@
 import time
-from Turtlebot.moveClass import moveClass
+#from Turtlebot.moveClass import moveClass
 from Protocol.DataLink.protocol_class import protocolClass
 from Protocol.Physical.DTMF_overclass import DTMF
 
@@ -16,16 +16,16 @@ def main():
     #data_prot.print()
     #time.sleep(100)
     
-    moveObj = moveClass()
+    #moveObj = moveClass()
     data = robot.listen.startListen()
     robot.send.compare(pack, robot.listen.outputList)
     data_prot = protocolClass(moves=data,robot=robot)
 
     data_prot.DataLinkUp()  
     data_prot.print()
-    for i in range(len(data_prot.data_list)-1):
-        moveObj.move(data_prot.data_list[i][0],data_prot.data_list[i][1])
-    moveObj.stop()
+    #for i in range(len(data_prot.data_list)-1):
+    #    moveObj.move(data_prot.data_list[i][0],data_prot.data_list[i][1])
+    #moveObj.stop()
 
 if __name__ == "__main__":
     main()
