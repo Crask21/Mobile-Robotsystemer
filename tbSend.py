3be806cbe4fed77a0131835da51870f4b8b36772
anpom21 <anpom21@student.sdu.dk>
2022-12-08 13:51:05 +0100

    aps

---
+++ tbSend.py
@@ -0,0 +1,33 @@
+import sys
+#import Turtlebot.Turtlebot_Python.moveClass as moveClass
+from Protocol.DataLink.protocol_class import protocolClass
+from Protocol.Physical.DTMF_overclass import DTMF
+import time
+
+
+def main():
+    baudrate = 50
+    sync = 30
+
+    robot=DTMF(baudrate,sync,mono_robot=True)
+    
+    #moveObj = moveClass.bot()
+    
+    move = [[0,20],[90,15]]
+
+    pack = protocolClass(move,robot,'output.txt')
+
+    pack.DataLinkDown()
+    #pack.print()
+    #pack.DataLinkUp()
+    print(pack.data_list)
+    
+    robot.send.send_package(pack.data_list)
+
+
+    
+    #moveObj.move(ang, dist)
+    #moveObj.stop()
+
+if __name__ == "__main__":
+    main()
