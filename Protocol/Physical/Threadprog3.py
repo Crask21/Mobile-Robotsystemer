d1e428e6596d9aee0c36a86f02d451d48e96d9d3
markus simonsen <markus.k.simonsen@gmail.com>
2022-12-14 13:09:50 +0100

    id

--- Protocol/Physical/Threadprog3.py
+++ Protocol/Physical/Threadprog3.py
@@ -264,7 +264,7 @@ class LISTEN():
         print(rec.warning)
         return rec.outputList
 
-roberto = LISTEN(20)
+roberto = LISTEN(50)
 output=roberto.startListen()
 pack=[0, 1, 10, 11, 12, 1, 8, 0, 9, 4, 12, 8, 2, 0, 1, 0, 1, 10, 11, 12, 2, 13, 10, 8, 15, 0, 5, 15, 0, 1, 0, 1, 10, 11, 12, 3, 4, 4, 6, 5, 6, 5, 7, 10, 2, 0, 6, 14, 2, 8, 9, 0, 1, 0, 1, 10, 11, 12, 4, 7, 5, 7, 4, 7, 3, 12, 10, 10, 0, 1]
 send.compare(pack,output)
