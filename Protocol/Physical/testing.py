b442fd8f4007bb70819058117f1c797c5c77dac0
anpom21 <anpom21@student.sdu.dk>
2022-12-15 12:39:27 +0100

    yessir

--- Protocol/Physical/testing.py
+++ Protocol/Physical/testing.py
@@ -23,14 +23,14 @@ dtmf_freq = [[1209,697], # 0
 
 # DTMF Settings
 fs = 44100
-amplitude = 5000
+amplitude = 6000
 fade_P = 0.003
-baud_rate = 50
-sync = 30
+baud_rate = 180
+sync = 70
 send = SEND(fs, amplitude, fade_P, baud_rate,sync)
 pack = [0, 1, 1, 0, 8, 3, 11, 13, 0, 1, 0, 1, 2, 4, 4, 6, 5, 6, 5, 7, 10, 2, 0, 6, 14, 4, 10, 2, 0, 1, 0, 1, 3, 7, 5, 7, 4, 7, 3, 14, 7, 0, 0, 1]
 
-pack = [*pack,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+pack = [*pack,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 
 send.send_package(pack)
