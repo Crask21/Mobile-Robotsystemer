c8d86458e40bbfb73f71372d02ca0973985d71e0
markus simonsen <markus.k.simonsen@gmail.com>
2022-12-15 12:33:40 +0100

    aps

--- Protocol/Physical/Threadprog4.py
+++ Protocol/Physical/Threadprog4.py
@@ -207,8 +207,8 @@ class LISTEN():
 
         return rec.result
 
-roberto = LISTEN(182)
-output=roberto.startListen()
-print(output)
-pack=[0, 1, 1, 0, 8, 3, 11, 13, 0, 1, 0, 1, 2, 4, 4, 6, 5, 6, 5, 7, 10, 2, 0, 6, 14, 4, 10, 2, 0, 1, 0, 1, 3, 7, 5, 7, 4, 7, 3, 14, 7, 0, 0, 1]
-send.compare(pack,output)
+#roberto = LISTEN(182)
+#output=roberto.startListen()
+#print(output)
+#pack=[0, 1, 1, 0, 8, 3, 11, 13, 0, 1, 0, 1, 2, 4, 4, 6, 5, 6, 5, 7, 10, 2, 0, 6, 14, 4, 10, 2, 0, 1, 0, 1, 3, 7, 5, 7, 4, 7, 3, 14, 7, 0, 0, 1]
+#send.compare(pack,output)
