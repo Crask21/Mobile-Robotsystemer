2afc004fa0e54700da1a4d0148bf0e70ff64da29
anpom21 <anpom21@student.sdu.dk>
2022-12-15 11:45:30 +0100

    AÅS

--- Protocol/Physical/testing.py
+++ Protocol/Physical/testing.py
@@ -30,7 +30,7 @@ sync = 30
 send = SEND(fs, amplitude, fade_P, baud_rate,sync)
 pack = [0, 1, 1, 0, 8, 3, 11, 13, 0, 1, 0, 1, 2, 4, 4, 6, 5, 6, 5, 7, 10, 2, 0, 6, 14, 4, 10, 2, 0, 1, 0, 1, 3, 7, 5, 7, 4, 7, 3, 14, 7, 0, 0, 1]
 
-pack = [*pack,0,0]
+pack = [*pack,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 
 send.send_package(pack)
