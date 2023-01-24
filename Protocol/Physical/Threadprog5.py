7894ad6b39ea55fc55184cd42345879fc646c38f
anpom21 <anpom21@student.sdu.dk>
2023-01-24 14:15:25 +0100

    aps

--- Protocol/Physical/Threadprog5.py
+++ Protocol/Physical/Threadprog5.py
@@ -33,8 +33,8 @@ class LISTEN():
                  channels = rec.CHANNELS,                          
                  rate = rec.RATE,                                  
                  input = True,
-                 frames_per_buffer=9999*2,
-                 input_device_index=1
+                 frames_per_buffer=9999*2#,
+                 #input_device_index=1
         )
 
         #for dtmf_to_hexa
