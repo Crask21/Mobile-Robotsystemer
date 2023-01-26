838a0798101213e39b6cd36e0151982c60bbcaca
anpom21 <anpom21@student.sdu.dk>
2023-01-26 13:34:07 +0100

    50 baud

--- MainSend.py
+++ MainSend.py
@@ -8,36 +8,25 @@ import time
 
 def main():
     baudrate = 50
-    sync = 30
+    sync = 10
 
     robot=DTMF(baudrate,sync)
     
-    #moveObj = moveClass.bot()
     
-    #move = [
-    #[0,20],[90,15]]
-    move = [[0,10],[90,0],[-90,0],[0,-10]]
+    move = [[-90,0],[-90,70],[90,30],[-90,50],[-30,100],[30,100]]   
 
-    #testpack=[0, 1, 1, 0, 8, 3, 11, 13, 0, 1, 0, 1, 2, 5, 3, 7, 5, 6, 3, 6, 3, 6, 5, 7, 3, 8, 6, 15, 0, 1, 0, 1, 3, 6, 6, 7, 5, 6, 12, 2, 0, 7, 2, 6, 15, 14, 10, 7, 0, 1, 0, 1, 4, 7, 5, 7, 4, 6, 5, 6, 11, 7, 0, 1]
-
-    #robot.send.send_package(testpack)
+   
     
     
 
-    pack = protocolClass(['0x7','0x0','0x8'],move,robot,'output.txt')
+    pack = protocolClass(['0xd','0x0','0x8'],move,robot,'output.txt')
 
     pack.DataLinkDown()
 
-    print(pack.data_list)
+    
     pack.PhysicalDown()
     
-    #robot.send.send_package(pack.data_list)
-    #robot.send.send_package([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],False)
-
-
-    
-    #moveObj.move(ang, dist)
-    #moveObj.stop()
+   
 
 if __name__ == "__main__":
     main()
